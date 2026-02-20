07abb43b5008198e17396152540b7a8bb3eb7b8c
LearnSwedish <johan.hofvendahl@gmail.com>
2026-02-20 13:18:13 +0100

    Add files via upload

---
+++ grammar-run-words.js
@@ -0,0 +1,15 @@
+// words.js
+export const words = [
+    { text: "ÄPPLE", type: "ett" },
+    { text: "BANAN", type: "en" },
+    { text: "HUS", type: "ett" },
+    { text: "BIL", type: "en" },
+    { text: "HUND", type: "en" },
+    { text: "BORD", type: "ett" },
+    { text: "APELSIN", type: "en" },
+    { text: "BLOMMA", type: "en" },
+    { text: "FRUKT", type: "en" },
+    { text: "PAPPA", type: "en" },
+    { text: "MAMMA", type: "en" },
+    { text: "FÄRG", type: "en" }
+];
