06028a26073a7f44b33f04e60748638971bae5da
LearnSwedish <johan.hofvendahl@gmail.com>
2026-02-20 13:22:46 +0100

    Create grammar-run.js

---
+++ grammar-run.js
@@ -0,0 +1,14 @@
+export const words = [
+    { text: "ÄPPLE", type: "ett" },
+    { text: "BANAN", type: "en" },
+    { text: "HUS", type: "ett" },
+    { text: "BIL", type: "en" },
+    { text: "HUND", type: "en" },
+    { text: "BORD", type: "ett" },
+    { text: "APELSIN", type: "en" },
+    { text: "BLOMMA", type: "en" },
+    { text: "FRUKT", type: "en" },
+    { text: "PAPPA", type: "en" },
+    { text: "MAMMA", type: "en" },
+    { text: "FÄRG", type: "en" }
+];
